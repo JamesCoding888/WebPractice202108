b420618504ba0d7181f498273b3ff391d9895ba4
jamesliao <JamesCoding888@hotmail.com>
2021-08-10 00:17:22 +0800

    javaWeb

--- WebPractice202108/src/main/java/controller/LoginController.java
+++ WebPractice202108/src/main/java/controller/LoginController.java
@@ -22,6 +22,7 @@ public class LoginController extends HttpServlet {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String username = req.getParameter("username");
